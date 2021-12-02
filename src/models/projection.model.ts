73f4dcea13c95fbd9025c7f40d771194ea79a4ba
Natalie Pilling <natalie.s.pilling@gmail.com>
2021-12-02 15:26:07 +0100

    feat: send base projections for savings, types and categories for next 12 months

--- src/models/projection.model.ts
+++ src/models/projection.model.ts
@@ -17,6 +17,9 @@ async function getAverageByType(
       _avg: {
         amount: true,
       },
+      _count: {
+        amount: true,
+      },
       where: {
         userId: {
           in: userIds,
@@ -36,20 +39,54 @@ async function getAverageByType(
   }
 }
 
+async function getAverageByCategory(
+  userIds: UserId[] | undefined,
+  category: string,
+  dateRange: DateRange
+) {
+  try {
+    // RETRIEVE AVERAGE _______ THROUGH PRISMA AGGREGATE
+    const aggregations = await prisma.transaction.aggregate({
+      _avg: {
+        amount: true,
+      },
+      _count: {
+        amount: true,
+      },
+      where: {
+        userId: {
+          in: userIds,
+        },
+        category: {
+          equals: category,
+        },
+        date: {
+          gte: dateRange.startDate,
+          lt: dateRange.endDate,
+        },
+      },
+    });
+    return aggregations._avg.amount;
+  } catch (err) {
+    console.error("ERROR: ", err);
+  }
+}
+
 // savingsCumulative
 
 // TRANSACTIONS ONLY - NO PROJECTIONS
 
 // AVERAGES
-// Get sum of all relevant transactions (by date & type)
-// Divide sum by X months
+// Get average of all relevant transactions (by date & type)
 
 // savings
-// income
-// expenses
-// categories
-//    rent
-//    ...
-//    others
 
-export default { getAverageByType };
+// types
+//   income
+//   expenses
+// categories
+//   rent
+//   ...
+//   others
+
+export default { getAverageByType, getAverageByCategory };
