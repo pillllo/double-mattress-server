67194d7a165d2f1d162cf95c7bdacb950a6e5abc
Natalie Pilling <natalie.s.pilling@gmail.com>
2021-12-02 17:53:51 +0100

    feat: adjust cumulative savings projection depending on queried date (#39)
    * feat: add get transactions and create transaction controller
    
    * docs: update readme to include create transaction
    
    * feat: add projection controller to calculate average income & expenses
    
    * feat: add helper function to check if user exists
    
    * refactor: remove comments
    
    * feat: add helper functions in user model to call getUser and getUserIds
    
    * refactor: refactor .env files
    
    * refactor: reorganize .env files into prod and dev
    
    * feat: send base projections for savings, types and categories for next 12 months
    
    * chore: comment back in secutiry code
    
    * chore: fix bracket problem
    
    * fix
    
    * feat: show cumulativeSavings projection depending on queried date

---
+++ src/types/projection.ts
@@ -0,0 +1,28 @@
+type Projection = {
+  savings: Savings;
+  typeAverages: TypeAverages;
+  categoryAverages: CategoryAverages;
+};
+
+type Savings = {
+  monthlyAverage3Months?: number;
+  totalSinceJoining?: number;
+};
+
+type TypeAverages = {
+  income?: number;
+  expense?: number;
+};
+
+type CategoryAverages = {
+  Salary?: number;
+  "Other Income"?: number;
+  "Bills and Services"?: number;
+  Home?: number;
+  Shopping?: number;
+  Entertainment?: number;
+  "Eating Out"?: number;
+  Others?: number;
+};
+
+export default Projection;
