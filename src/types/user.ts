21c5f0fd000953d3b5adba0958219a3f9ad58d1c
Natalie Pilling <natalie.s.pilling@gmail.com>
2021-12-07 19:14:50 +0100

    feat: add new user property for subscriptions

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,8 +3,10 @@ import { UserId } from "./id";
 type User = {
   userId: UserId;
   firstName: string;
+  email: string;
   currency: string;
   linkedUserIds: string[];
+  activeSubscription: boolean;
 };
 
 export default User;
