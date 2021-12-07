71ba76fd1397ea3aa78963da33403061bd9984e8
James Foxlee <james.foxlee@protonmail.com>
2021-12-07 14:41:06 +0000

    feature: connect/initiate

--- src/controllers/connection.controller.ts
+++ src/controllers/connection.controller.ts
@@ -1,17 +1,26 @@
 import { Request, Response } from "express";
+import EmailValidator from "email-validator";
 
 import UserModel from "../models/user.model";
 import ConnectionModel from "../models/connection.model";
 
 async function initiateConnect(req: Request, res: Response): Promise<void> {
   try {
+    console.log("ConnectionController.initiateConnect()");
     const { userId, email } = req.body;
-    console.log("ConnectionController.initiateConnect() for userId: ", userId);
-    // TODO: implement
-    res.status(418).send();
+    const user = await UserModel.getUser(userId);
+    const isEmail = EmailValidator.validate(email);
+    if (!user || !isEmail || email === user.email) throw "Invalid request";
+    const userToConnect = await UserModel.getUserByEmail(email);
+    if (!userToConnect) throw "Invalid request";
+    // don't want to return userId - security issues
+    res.status(200).send({
+      firstName: userToConnect.firstName,
+      email: userToConnect.email,
+    });
   } catch (err) {
     console.error(err);
-    res.status(400).send("Could not connect");
+    res.status(400).send(err);
   }
 }
 
--- src/types/user.ts
+++ src/types/user.ts
@@ -3,6 +3,7 @@ import { UserId } from "./id";
 type User = {
   userId: UserId;
   firstName: string;
+  email: string;
   currency: string;
   linkedUserIds: string[];
 };
