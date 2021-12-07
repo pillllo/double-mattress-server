bf114b7eb4ee125249acf9d8846e25c9718910ce
Natalie Pilling <natalie.s.pilling@gmail.com>
2021-12-07 19:04:43 +0100

    merge

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ function bootstrapServer() {
   }
   // correct .env file has been found, now load it
   require("custom-env").env(environment);
-  const { PORT } = process.env;
+  const { PORT } = process.env || 3001;
 
   // 1. create Express app instance, configure static assets & routes
   const expressApp = express();
